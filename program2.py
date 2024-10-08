5960fcbb57052d3b6d3586bd18d77d81cbd27a05
anish97081 <97081anish@gmail.com>
2024-10-08 15:05:58 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,26 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        
+        # Mapping of Roman numerals to their integer values
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        # Initialize the total sum
+        total = 0
+        
+        # Iterate over the string, checking each numeral
+        for i in range(len(s)):
+            # If the current numeral is smaller than the next one, subtract it
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, add it
+                total += roman_map[s[i]]
+        
+        return total
+    pass
 
 
 
